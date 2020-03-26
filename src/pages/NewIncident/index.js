380c77d06c029277b24bd4b426d15c598a7c6e05
Daniel <daniel_sousa.p@outlook.com>
2020-03-26 00:01:41 -0300

    feat:Finish web app

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import './styles.css';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function NewIncident() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const history = useHistory();
+
+  const ongId = localStorage.getItem('ongId');
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+    
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId,
+        }
+      });
+
+      history.push('/profile');
+    } catch(err) {
+      alert('Erro ao cadastrar o caso, tente novamente!');
+    }
+  }
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -22,10 +53,22 @@ export default function NewIncident() {
           </Link>
         </section>
 
-        <form>
-          <input placeholder="Título do caso" />
-          <textarea placeholder="Descrição" />
-          <input placeholder="Valor em reais" />
+        <form onSubmit={handleNewIncident}>
+          <input 
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            placeholder="Título do caso"
+          />
+          <textarea 
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+            placeholder="Descrição"
+          />
+          <input 
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            placeholder="Valor em reais"
+          />
           
           <button className="button" type="submit">
             Cadastrar
